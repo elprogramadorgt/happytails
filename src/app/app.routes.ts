950a2496d90bc043f34e958c4e84c96a0bce8793
Edu <elprogramadorgt8@gmail.com>
2023-12-22 21:30:01 -0600

    ++ routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DogComponent } from './dog/dog.component';
+import { AddDogComponent } from './add-dog/add-dog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+  { path: 'dog', component: DogComponent },
+  { path: 'dog/add', component: AddDogComponent },
+];
